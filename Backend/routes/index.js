c2396734a6f2525ebab07153197d9151079b7af1
Ayush Kumar Ray <ak05078189@gmail.com>
2025-12-18 15:41:44 +0530

    added

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -17,7 +17,8 @@ router.post("/send-email", async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
